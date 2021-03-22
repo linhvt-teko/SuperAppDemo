2e8878a4e4517e40f59475774d28f62c9d20beb9
Nguyễn Xuân Tùng <tung.nx@teko.vn>
2021-03-22 13:33:39 +0700

    Update hotel connector

---
+++ SuperAppDemo/Tripi/TripiHotelConnector.framework/Headers/TripiHotelConnector.h
@@ -0,0 +1,18 @@
+//
+//  TripiHotelConnector.h
+//  TripiHotelConnector
+//
+//  Created by Tung Nguyen on 3/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TripiHotelConnector.
+FOUNDATION_EXPORT double TripiHotelConnectorVersionNumber;
+
+//! Project version string for TripiHotelConnector.
+FOUNDATION_EXPORT const unsigned char TripiHotelConnectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TripiHotelConnector/PublicHeader.h>
+
+
